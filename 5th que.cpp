6fff884231d6eb784b6d374a7899525ed14c301f
Varshiiiiiiiiiiiiiiiiiii <128604973+Varshiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2023-04-06 07:23:08 +0530

    Add files via upload

---
+++ 5th que.cpp
@@ -0,0 +1,11 @@
+too many types-error float x;or double x;-correction
+type must be matched-error char *cp=(char*)vp;
+no error
+syntax error int*p=new int[10];-correction
+address have to assign instead of content-error int const*p=&total;-correction
+c++ requiores a const to be initialized-error const in arraysize=5;-correction
+invalid variable name-error int number =100;-correction
+wrong data type float*p=2345
+wrong 
+array size of char must be larger than number of characters in string charname[4]="welcome";
+
